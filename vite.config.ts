7971128181230fbd915e2b58f537094b94e6da50
r.trofimenko <r.trofimenko@net-vision.pro>
2023-08-26 18:38:49 +0300

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/cifrovoi-proriv-spb/',
+  base: '/cifrovoi_proriv_spb/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
